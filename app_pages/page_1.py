3a9720d26b855a9151957eddd55ed909bbc39ce5
Beth Cottell <cottellbc@gmail.com>
2025-04-28 13:27:13 +0000

    added text to Page 1 (P1) in multipage app, removed placeholder text P1.

--- app_pages/page_1.py
+++ app_pages/page_1.py
@@ -2,4 +2,40 @@ import streamlit as st
 
 
 def page1_body():
-    st.write("This is page 1")
+    st.success("Project Summary" \
+    "Data source:'https://www.kaggle.com/codeinstitute/cherry-leaves'"
+    "Readme Link: 'https://github.com/BeeBeeBethC/PP5-Cherry-Leaves'"
+    )
+    st.write("For the purpose of this project, A fictional business was " \
+    "created. Farmy & Foods, a company in the agricultural " \
+    "sector, produces and harvests different types of food. " \
+    "Recently, they are facing a challenge where their cherry plantations " \
+    "have been presenting with powdery mildew." \
+    )
+    st.write(
+    "Powdery Mildew is a fungal disease which affects plants and crops. " \
+    "The cherry plantation belonging to Farmy and Foods, is one of their " \
+    "finest products in their portfolio." \
+    "Farmy and Foods are concerned about supplying the market with a " \
+    "product of compromised quality. "
+    )
+    st.write(
+    "Currently, the process is to manually verify if a given cherry tree " \
+    "contains powdery mildew. " \
+    "As a result, this manual process is not scalable due to it being a " \
+    "lengthy process. " \
+    "To save time in this process, the IT team suggested an Machine Learning " \
+    "system that is capable of detecting Powdery Mildews in Cherry Leaves. " \
+    "Using a tree leaf image, to identify if the leaf is healthy or has " \
+    "powdery mildew present. "
+    )
+    
+    st.success("Business Requirements")
+    st.write(
+    "1. The client is interested in conducting a study " \
+    "to visually differentiate a cherry leaf that is healthy from one that " \
+    "contains powdery mildew." \
+    )
+    st.write(
+    "2. The client is interested in predicting if a cherry leaf is healthy " \
+    "or contains powdery mildew.")
--- multipage-mildew-app.py
+++ multipage-mildew-app.py
@@ -9,7 +9,7 @@ from app_pages.page_5 import page5_body
 
 app = MultiPage(app_name= "Mildew Detection in Cherry Leaves")
 
-app.app_page("Page 1", page1_body)
+app.app_page("Project Summary", page1_body)
 app.app_page("Page 2", page2_body)
 app.app_page("Page 3", page3_body)
 app.app_page("Page 4", page4_body)
