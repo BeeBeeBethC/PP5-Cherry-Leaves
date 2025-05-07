fca8895f760e6f5fc06caf0ca0810a65e2783241
Beth Cottell <cottellbc@gmail.com>
2025-05-07 12:48:20 +0000

    adds paths to app-pages

--- app_pages/page_5.py
+++ app_pages/page_5.py
@@ -10,7 +10,7 @@ def page5_body():
 
     # Confusion Matrix
     st.markdown("### Confusion Matrix")
-    cm_path = "/workspaces/PP5-Cherry-Leaves/outputs/figures/confusion_matrix.png"
+    cm_path = f"outputs/figures/confusion_matrix.png"
     if os.path.exists(cm_path):
         cm_img = Image.open(cm_path)
         st.image(cm_img, caption="Confusion Matrix", use_container_width=True)
@@ -19,7 +19,7 @@ def page5_body():
 
     def display_classification_report():
         # Path to the classification report CSV file
-        csv_path = "/workspaces/PP5-Cherry-Leaves/outputs/saved_results/classification_report.csv"
+        csv_path = "outputs/saved_results/classification_report.csv"
 
         # Check if the CSV file exists
         if os.path.exists(csv_path):
@@ -37,7 +37,7 @@ def page5_body():
 
     # ROC Curve and AUC Score
     st.header("ROC Curve & AUC Score")
-    roc_img_path = "/workspaces/PP5-Cherry-Leaves/outputs/training_plots/roc_curve.png"
+    roc_img_path = "outputs/training_plots/roc_curve.png"
     if os.path.exists(roc_img_path):
         roc_img = Image.open(roc_img_path)
         st.image(roc_img, caption="ROC Curve", use_container_width=True)
@@ -57,7 +57,7 @@ def page5_body():
     Below is a sample batch of images along with predictions to see how the 
     model would perform on unseen data.
     """)
-    sample_img_path = "/workspaces/PP5-Cherry-Leaves/outputs/figures/sample_predictions.png"
+    sample_img_path = "outputs/figures/sample_predictions.png"
     if os.path.exists(sample_img_path):
         sample_img = Image.open(sample_img_path)
         st.image(sample_img, caption="Test Samples", use_container_width=True)
--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -77,7 +77,7 @@ def load_model_and_predict(my_image, version):
     Load and perform ML prediction over live images
     """
 
-    model = load_model(f"/workspaces/PP5-Cherry-Leaves/outputs/models/cherry_leaf_model.h5")
+    model = load_model(f"outputs/models/cherry_leaf_model.h5")
 
     pred_proba = model.predict(my_image)[0, 0]
 
