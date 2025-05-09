40deb0f1eecfa81f9839a3586564bfcf46b8478f
Beth Cottell <cottellbc@gmail.com>
2025-05-09 10:14:02 +0000

    adds pep8 compliancy to app.py, multipage_app.py and page_1.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import streamlit as st
 from app_pages.multipage_app import MultiPage
-
 from app_pages.page_1 import page1_body
 from app_pages.page_2 import page2_body
 from app_pages.page_3 import page3_body
 from app_pages.page_4 import page4_body
 from app_pages.page_5 import page5_body
 
-app = MultiPage(app_name= "Mildew Detection in Cherry Leaves")
+st.set_page_config(
+    page_title="Mildew Detection in Cherry Leaves",
+    layout="wide"
+)
+
+st.title("Mildew Detection in Cherry Leaves")
+
+app = MultiPage(app_name="Mildew Detection in Cherry Leaves")
 
 app.app_page("Project Summary", page1_body)
 app.app_page("Visualizations", page2_body)
-app.app_page("Predictions", page3_body)
-app.app_page("Hypothesis", page4_body)
+app.app_page("Mildew Detection", page3_body)
+app.app_page("Hypothesis and Validation", page4_body)
 app.app_page("Metrics and Evaluation", page5_body)
 
 app.run()
--- app_pages/page_1.py
+++ app_pages/page_1.py
@@ -3,28 +3,45 @@ import streamlit as st
 
 def page1_body():
 
-    st.write(
-        f"(For the purpose of this project, a fictional business was created.) \n\n"
-        f"Farmy & Foods, a fictional company in the agricultural sector, produces and harvests various types of food. "
-        f"Recently, they have been facing a challenge: their cherry plantations are affected by powdery mildew."
-    )
-    st.write(
-        f"Powdery mildew is a fungal disease that affects plants and crops. "
-        f"The cherry plantation at Farmy & Foods is one of their most prized products, and they are concerned about its "
-        f"quality due to this disease."
-    )
-    st.write(
-        f"Currently, the process of detecting powdery mildew is manual and time-consuming, with each tree taking about 30 minutes to inspect. "
-        f"This process is not scalable, especially given the large number of trees in the plantation. \n\n"
-        f"To solve this problem, the IT team suggested implementing a Machine Learning system capable of automatically detecting "
-        f"powdery mildew in cherry leaves by analyzing images of the leaves to classify them as either healthy or infected."
-    )
+    st.write("""
+A fictional business was created for the purpose of this project.
+""")
 
-    st.success("### Business Requirements\n\n"
-               f"1. The client wants to conduct a study to visually differentiate healthy cherry leaves from those affected by powdery mildew.\n\n"
-               f"2. The client seeks a solution that can predict whether a cherry leaf is healthy or infected with powdery mildew."
-               )
 
-    st.info("### More Information\n\n"
-            f"Further details can be found in this README file: [link](https://github.com/BeeBeeBethC/PP5-Cherry-Leaves)\n"
-            )
+st.write("""
+Farmy & Foods produces and harvests various types of food.
+Recently, they have been facing a challenge: their cherry
+plantations are affected by powdery mildew.
+
+Powdery mildew is a fungal disease that affects plants and crops.
+The cherry plantation at Farmy & Foods is one of their most prized products,
+and they are concerned about its quality due to this disease.
+""")
+
+st.write("""
+Currently, the process of detecting powdery mildew is manual and
+time-consuming, with each tree taking about 30 minutes to inspect.
+This process is not scalable,
+especially given the large number of trees in the plantation.
+
+To solve this problem, the IT team suggested implementing a Machine Learning
+system capable of automatically detecting powdery mildew in cherry leaves by
+analyzing images of the leaves to classify them as either healthy or infected.
+""")
+
+st.success("""
+### Business Requirements
+
+1. The client wants to conduct a study to visually differentiate healthy
+cherry leaves from those affected by powdery mildew.
+
+2. The client seeks a solution that can predict whether a cherry leaf is
+healthy or infected with powdery mildew.
+""")
+
+st.info("""
+### More Information
+
+Further details can be found in this
+[README](https://github.com/BeeBeeBethC/PP5-Cherry-Leaves)
+""")
